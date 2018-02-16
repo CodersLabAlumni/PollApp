b9fb9218935d7ea5780c40a1bfe1046799ef6bb9
mateuszlichon <mateusz.lichon@gmail.com>
2018-02-16 12:00:37 +0100

    sets up basic view

--- src/main/webapp/WEB-INF/js/app.js
+++ src/main/webapp/WEB-INF/js/app.js
@@ -1,28 +1,28 @@
 $(function () {
-  var baseUrl = 'http://localhost:5000/'
-  var factories = $('ul.factories')
-
-  test();
-
-  function test() {
-    factories.append('<li> test </li>');
-  }
-
-  renderList();
-
-  function renderList() {
-    $.getJSON({
-      url: baseUrl + 'factory/list/'
-    }).done(function (data) {
-      console.log(data);
-      factories.empty();
-      data.forEach(function (factory) {
-        factories.append('<li>' + factory.supervisor.firstName + '</li>');
-      })
-    }).fail(function () {
-      console.log("error");
-    });
-  }
+  // var baseUrl = 'http://localhost:5000/'
+  // var factories = $('ul.factories')
+  //
+  // test();
+  //
+  // function test() {
+  //   factories.append('<li> test </li>');
+  // }
+  //
+  // renderList();
+  //
+  // function renderList() {
+  //   $.getJSON({
+  //     url: baseUrl + 'factory/list/'
+  //   }).done(function (data) {
+  //     console.log(data);
+  //     factories.empty();
+  //     data.forEach(function (factory) {
+  //       factories.append('<li>' + factory.supervisor.firstName + '</li>');
+  //     })
+  //   }).fail(function () {
+  //     console.log("error");
+  //   });
+  // }
 
   var pollCreate = $('#pollCreate');
   var pollForm = $('#pollForm');
@@ -31,43 +31,92 @@ $(function () {
     pollForm.toggle();
   })
 
-  jQuery(function ($) {
-      var data1 = [12, 10, 20, 40, 30, 60, 50];
-      var data2 = [70, 12, 8, 10];
+  var button = $('.form-check-input');
+
+  button.on('click', function () {
+    console.log($(this).parents('.text-white'));
+    $(this).parents('.text-white').fadeOut();
+  })
 
 
-      $("#chart1").shieldChart({
-          exportOptions: {
-              image: false,
-              print: false
-          },
-          axisY: {
-              title: {
-                  text: "Break-Down for selected quarter"
-              }
-          },
-          dataSeries: [{
-              seriesType: "bar",
-              data: data1
-          }]
-      });
+      var chart = new CanvasJS.Chart("chartContainer1", {
+	animationEnabled: true,
+	theme: "light2", // "light1", "light2", "dark1", "dark2"
+	title:{
+		text: ""
+	},
+	axisY: {
+		title: "%"
+	},
+	data: [{
+		type: "column",
+		showInLegend: true,
+		legendMarkerColor: "grey",
+		legendText: "100 answers",
+		dataPoints: [
+			{ y: 10, label: "Philosopher's Stone" },
+			{ y: 7,  label: "Chamber of Secrets" },
+			{ y: 13,  label: "Prisoner of Azkaban" },
+			{ y: 15,  label: "Goblet of Fire" },
+			{ y: 20,  label: "Order of the Phoenix" },
+			{ y: 25, label: "Half-Blood Prince" },
+			{ y: 30,  label: "Deathly Hallows" }
+		]
+	}]
+});
+chart.render();
 
-      $("#chart2").shieldChart({
-          exportOptions: {
-              image: false,
-              print: false
-          },
-          axisY: {
-              title: {
-                  text: "Break-Down for selected quarter"
-              }
-          },
-          dataSeries: [{
-              seriesType: "bar",
-              data: data2
-          }]
-      });
-  });
+var chart = new CanvasJS.Chart("chartContainer2", {
+animationEnabled: true,
+theme: "light2", // "light1", "light2", "dark1", "dark2"
+title:{
+text: ""
+},
+axisY: {
+title: "%"
+},
+data: [{
+type: "column",
+showInLegend: true,
+legendMarkerColor: "grey",
+legendText: "138 answers",
+dataPoints: [
+{ y: 50, label: "YES" },
+{ y: 4,  label: "NO" },
+{ y: 20,  label: "Don't know, don't care" },
+{ y: 26,  label: "Who is Donald Trump" }
+]
+}]
+});
+chart.render();
+
+var chart = new CanvasJS.Chart("chartContainer3", {
+animationEnabled: true,
+theme: "light2", // "light1", "light2", "dark1", "dark2"
+title:{
+text: ""
+},
+axisY: {
+title: "%"
+},
+data: [{
+type: "column",
+showInLegend: true,
+legendMarkerColor: "grey",
+legendText: "20 answers",
+dataPoints: [
+{ y: 80, label: "Venezuela" },
+{ y: 70,  label: "Saudi" },
+{ y: 60,  label: "Canada" },
+{ y: 50,  label: "Iran" },
+{ y: 40,  label: "Iraq" },
+{ y: 30, label: "Kuwait" },
+{ y: 20,  label: "UAE" },
+{ y: 10,  label: "Russia" }
+]
+}]
+});
+chart.render();
 
   // function renderList() {
   //   $.ajax({
