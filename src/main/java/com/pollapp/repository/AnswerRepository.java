d0a41b1c76b9e7d8161a0b50a5d1947182d1892d
przemyslaw <przemyslawkonik@gmail.com>
2018-02-22 14:20:32 +0100

    add methods to repositories

--- src/main/java/com/pollapp/repository/AnswerRepository.java
+++ src/main/java/com/pollapp/repository/AnswerRepository.java
@@ -4,6 +4,10 @@ import com.pollapp.entity.Answer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
+
+    List<Answer> findByPollId(long id);
 }
