80f9c3508fcadf35ab15d5cb3411e54835280a23
przemyslaw <przemyslawkonik@gmail.com>
2018-03-02 17:13:52 +0100

    clean, update

--- src/main/java/com/pollapp/repository/PollRepository.java
+++ src/main/java/com/pollapp/repository/PollRepository.java
@@ -1,24 +1,28 @@
 package com.pollapp.repository;
 
-import java.util.Calendar;
-import java.util.List;
-
+import com.pollapp.entity.Poll;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.pollapp.entity.Poll;
+import java.util.Calendar;
+import java.util.List;
 
 @Repository
 public interface PollRepository extends JpaRepository<Poll, Long> {
 
-	List<Poll> findAllByClosedAfter(Calendar date);
-	Page<Poll> findAllByClosedBeforeOrClosedIsNull(Calendar date, Pageable pageable);
-	List<Poll> findAllByCategoriesId(int categoryId);
-	Page<Poll> findAllByCategoriesId(int categoryId, Pageable pageable);
-	List<Poll> findAllByCategoriesIdAndClosedAfter(int categoryId, Calendar instance);
-	List<Poll> findAllByClosedBeforeOrClosedIsNullAndCategoriesId(Calendar instance, int categoryId);
-	List<Poll> findByIdNotIn(List<Long> pollIdList);
-  List<Poll> findByIdNotInAndCategoriesId(List<Long> pollIdList, int categoryId);
+    Page<Poll> findAllByClosedBeforeOrClosedIsNull(Calendar date, Pageable pageable);
+
+    Page<Poll> findAllByCategoriesId(int categoryId, Pageable pageable);
+
+    Page<Poll> findAllByCategoriesIdAndClosedAfter(int categoryId, Calendar instance, Pageable pageable);
+
+    Page<Poll> findAllByCategoriesIdAndClosedAfterAndIdNotIn(int categoryId, Calendar instance, List<Long> pollIdList, Pageable pageable);
+
+    Page<Poll> findAllByClosedAfter(Calendar instance, Pageable pageable);
+
+    Page<Poll> findAllByClosedAfterAndIdNotIn(Calendar instance, List<Long> pollIdList, Pageable pageable);
+
+    Page<Poll> findAllByClosedBeforeOrClosedIsNullAndCategoriesId(Calendar instance, int categoryId, Pageable pageable);
 }
