d4d2d7d1e8d4059795e1cdd359d29fad3b4a90db
przemyslaw <przemyslawkonik@gmail.com>
2018-03-14 14:04:33 +0100

    implement user details service

--- src/main/java/com/pollapp/service/AccountServiceImpl.java
+++ src/main/java/com/pollapp/service/AccountServiceImpl.java
@@ -1,6 +1,7 @@
 package com.pollapp.service;
 
 import com.pollapp.entity.UserAccount;
+import com.pollapp.entity.wrapper.CustomUserDetails;
 import com.pollapp.enums.Role;
 import com.pollapp.repository.UserAccountRepository;
 import com.pollapp.repository.UserRoleRepository;
@@ -8,11 +9,14 @@ import com.pollapp.response.RegisterResponse;
 import com.pollapp.validation.RegisterValidation;
 import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.BindingResult;
 
 @Service
-public class AccountServiceImpl implements AccountService {
+public class AccountServiceImpl implements AccountService, UserDetailsService {
 
     @Autowired
     RegisterValidation registerValidation;
@@ -31,5 +35,10 @@ public class AccountServiceImpl implements AccountService {
             userAccountRepository.save(userAccount);
         }
         return registerValidation.getRegisterResponse();
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return new CustomUserDetails(userAccountRepository.findByUsername(s));
     }
 }
