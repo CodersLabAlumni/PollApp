bb6839fc3b6374a798cb65173a48bf2d2ebeec52
przemyslaw <przemyslawkonik@gmail.com>
2018-03-21 12:08:45 +0100

    handle login success, add comment button

--- src/main/webapp/WEB-INF/resources/js/app_logged.js
+++ src/main/webapp/WEB-INF/resources/js/app_logged.js
@@ -1,11 +1,26 @@
 $(function () {
 
+    var ajax = new Ajax();
+
     function handleLoginSuccess() {
         var cookie = Cookies.get('logged_user');
         if (cookie !== undefined) {
             $('#username-logged').text(cookie);
         }
     }
+
+    function commentPanel() {
+        return '<div class="container comment-add">' +
+            '<button>Add comment</button></div>'
+    }
+
+    $('.comment-add').on('click', 'button', function (e) {
+        e.preventDefault();
+    });
+
+    $('#closedPolls').children().each(function (index, elem) {
+        $(elem).append(commentPanel());
+    });
 
     handleLoginSuccess();
 
