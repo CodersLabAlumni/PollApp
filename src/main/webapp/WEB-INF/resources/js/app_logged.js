33c4cba129c6eb10cd4f1411b335f3c66d763cfe
przemyslaw <przemyslawkonik@gmail.com>
2018-03-22 18:14:42 +0100

    add comment functionality

--- src/main/webapp/WEB-INF/resources/js/app_logged.js
+++ src/main/webapp/WEB-INF/resources/js/app_logged.js
@@ -1,6 +1,7 @@
 $(function () {
 
     var ajax = new Ajax();
+    var closedPolls = $('#closedPolls');
 
     function handleLoginSuccess() {
         var cookie = Cookies.get('logged_user');
@@ -9,17 +10,29 @@ $(function () {
         }
     }
 
-    function commentPanel() {
-        return '<div class="container comment-add">' +
-            '<button>Add comment</button></div>'
+    function renderAddCommentForm() {
+        return '<div class="container comment-add text-center">' +
+            '<textarea class="comment-text form-control" placeholder="Insert text..."></textarea><br>' +
+            '<a class="comment-add-btn btn">Add comment</a>' +
+            '</div>'
     }
 
-    $('.comment-add').on('click', 'button', function (e) {
+    closedPolls.on('click', '.comment-add-btn', function (e) {
         e.preventDefault();
+        var textNode = $(e.target).siblings('.comment-text');
+        var text = textNode.val();
+        var pollId = $(e.target).parent().siblings('.toggle-comments').find('a').data('poll');
+        var comment = {content: text};
+        ajax.ajaxPost('/polls/' + pollId + '/comments', comment);
+        textNode.val('');
     });
 
-    $('#closedPolls').children().each(function (index, elem) {
-        $(elem).append(commentPanel());
+    closedPolls.on('click', '.toggle-comments', function (e) {
+        var commentAddNode = $(e.target).parent().siblings('.comment-add');
+        if (commentAddNode.length === 0) {
+            $(e.target).parent().siblings('.pager-comments').after(renderAddCommentForm());
+        }
+        commentAddNode.toggle('hidden');
     });
 
     handleLoginSuccess();
